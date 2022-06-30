a637a939b0964372dc639c55387783b0771caf55
Daniel Jilg <daniel@telemetrydeck.com>
2022-06-30 11:25:01 +0200

    Rework Aggregators Representation

---
+++ Tests/DataTransferObjectsTests/AggregatorTests.swift
@@ -0,0 +1,82 @@
+@testable import DataTransferObjects
+import XCTest
+
+final class AggregatorTests: XCTestCase {
+    let exampleDruidAggregatorsThetaSketch = """
+    [
+        {
+          "fieldName": "clientUser",
+          "name": "count",
+          "type": "thetaSketch"
+        }
+      ]
+    """
+    .filter { !$0.isWhitespace }
+    .data(using: .utf8)!
+    
+    let exampleDruidAggregatorsCountSum = """
+    [
+        {
+          "fieldName": "count",
+          "name": "count",
+          "type": "longSum"
+        }
+      ]
+    """
+    .filter { !$0.isWhitespace }
+    .data(using: .utf8)!
+
+    let exampleDruidAggregatorsFiltered = """
+    [
+        {
+          "type": "filtered",
+          "filter": {
+            "type": "selector",
+            "dimension": "type",
+            "value": "newSessionBegan"
+          },
+          "aggregator": {
+            "type": "thetaSketch",
+            "name": "newSessionBegan",
+            "fieldName": "clientUser"
+          }
+        },
+        {
+          "type": "filtered",
+          "filter": {
+            "type": "and",
+            "fields": [
+              {
+                "type": "selector",
+                "dimension": "type",
+                "value": "InsightShown"
+              }
+            ]
+          },
+          "aggregator": {
+            "type": "thetaSketch",
+            "name": "InsightShown",
+            "fieldName": "clientUser"
+          }
+        }
+      ]
+    """
+    .filter { !$0.isWhitespace }
+    .data(using: .utf8)!
+    
+    func testThetaSketchAggregatorDecoding() throws {
+        let decodedAggregators = try JSONDecoder.telemetryDecoder.decode([Aggregator].self, from: exampleDruidAggregatorsThetaSketch)
+        
+        XCTAssertEqual(decodedAggregators, [Aggregator.thetaSketch(.init(type: .thetaSketch, name: "count", fieldName: "clientUser"))])
+    }
+    
+    func testLongSumAggregatorDecoding() throws {
+        let decodedAggregators = try JSONDecoder.telemetryDecoder.decode([Aggregator].self, from: exampleDruidAggregatorsCountSum)
+        
+        XCTAssertEqual(decodedAggregators, [Aggregator.longSum(.init(type: .longSum, name: "count", fieldName: "count"))])
+    }
+
+    func testFilteredAggregatorDecoding() throws {
+        let decodedAggregators = try JSONDecoder.telemetryDecoder.decode([Aggregator].self, from: exampleDruidAggregatorsFiltered)
+    }
+}
