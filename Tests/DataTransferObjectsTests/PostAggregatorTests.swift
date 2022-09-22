4ead71c2ada727c2617bc3fee2e4726b8de4c5a8
Daniel Jilg <daniel@telemetrydeck.com>
2022-09-22 20:38:09 +0200

    Inline Test Examples for better overview

--- Tests/DataTransferObjectsTests/PostAggregatorTests.swift
+++ Tests/DataTransferObjectsTests/PostAggregatorTests.swift
@@ -9,85 +9,60 @@
 import XCTest
 
 final class PostAggregatorTests: XCTestCase {
-    let examplePostAggregatorThetaSketchEstimate = """
-    [
-      {
-        "type": "thetaSketchEstimate",
-        "name": "app_launched_and_data_entered_count",
-        "field": {
-          "type": "thetaSketchSetOp",
-          "name": "app_launched_and_data_entered_count",
-          "func": "INTERSECT",
-          "fields": [
-            {
-              "type": "fieldAccess",
-              "fieldName": "appLaunchedByNotification_count"
-            },
-            {
-              "type": "fieldAccess",
-              "fieldName": "dataEntered_count"
-            }
-          ]
-        }
-      }
-    ]
-    """
-    .filter { !$0.isWhitespace }
-    .data(using: .utf8)!
-
-    let examplePostAggregatorArithmetic = """
-    [{
-        "type"   : "arithmetic",
-        "name"   : "average",
-        "fn"     : "/",
-        "fields" : [
-               { "type" : "fieldAccess", "name" : "tot", "fieldName" : "tot" },
-               { "type" : "fieldAccess", "name" : "rows", "fieldName" : "rows" }
-             ]
-      }]
-    """
-    .filter { !$0.isWhitespace }
-    .data(using: .utf8)!
-
-    let examplePostAggregatorPercentage = """
-    [{
-        "type"   : "arithmetic",
-        "name"   : "part_percentage",
-        "fn"     : "*",
-        "fields" : [
-           { "type"   : "arithmetic",
-             "name"   : "ratio",
-             "fn"     : "/",
-             "fields" : [
-               { "type" : "fieldAccess", "name" : "part", "fieldName" : "part" },
-               { "type" : "fieldAccess", "name" : "tot", "fieldName" : "tot" }
-             ]
-           },
-           { "type" : "constant", "name": "const", "value" : 100 }
-        ]
-      }]
-    """
-    .filter { !$0.isWhitespace }
-    .data(using: .utf8)!
-
-    let examplePostAggregatorExpression = """
-    [{
-        "type"       : "expression",
-        "name"       : "part_percentage",
-        "expression" : "100 * (part / tot)"
-      }]
-    """
-    .filter { !$0.isWhitespace }
-    .data(using: .utf8)!
-
-
     func testThetaSketchAggregatorDecoding() throws {
+        let examplePostAggregatorThetaSketchEstimate = """
+        [
+          {
+            "type": "thetaSketchEstimate",
+            "name": "app_launched_and_data_entered_count",
+            "field": {
+              "type": "thetaSketchSetOp",
+              "name": "app_launched_and_data_entered_count",
+              "func": "INTERSECT",
+              "fields": [
+                {
+                  "type": "fieldAccess",
+                  "fieldName": "appLaunchedByNotification_count"
+                },
+                {
+                  "type": "fieldAccess",
+                  "fieldName": "dataEntered_count"
+                }
+              ]
+            }
+          }
+        ]
+        """
+        .filter { !$0.isWhitespace }
+        .data(using: .utf8)!
+
         let decodedAggregators = try JSONDecoder.telemetryDecoder.decode([PostAggregator].self, from: examplePostAggregatorThetaSketchEstimate)
 
         XCTAssertEqual(decodedAggregators, [PostAggregator.arithmetic(ArithmetricPostAggregator(name: "part_percentage", function: .multiplication, fields: []))])
     }
 
     func testPercentageArithmeticDecoding() throws {
+        let examplePostAggregatorPercentage = """
+        [{
+            "type"   : "arithmetic",
+            "name"   : "part_percentage",
+            "fn"     : "*",
+            "fields" : [
+               { "type"   : "arithmetic",
+                 "name"   : "ratio",
+                 "fn"     : "/",
+                 "fields" : [
+                   { "type" : "fieldAccess", "name" : "part", "fieldName" : "part" },
+                   { "type" : "fieldAccess", "name" : "tot", "fieldName" : "tot" }
+                 ]
+               },
+               { "type" : "constant", "name": "const", "value" : 100 }
+            ]
+          }]
+        """
+        .filter { !$0.isWhitespace }
+        .data(using: .utf8)!
+
         let decodedAggregators = try JSONDecoder.telemetryDecoder.decode([PostAggregator].self, from: examplePostAggregatorPercentage)
 
         XCTAssertEqual(
@@ -111,8 +86,56 @@ final class PostAggregatorTests: XCTestCase {
         )
     }
 
-    func testExpressionDecoding() throws {
+    func testPostAggregatorExpressionDecoding() throws {
+        let examplePostAggregatorExpression = """
+        [{
+            "type"       : "expression",
+            "name"       : "part_percentage",
+            "expression" : "100 * (part / tot)"
+          }]
+        """
+        .filter { !$0.isWhitespace }
+        .data(using: .utf8)!
+
         let decodedAggregators = try JSONDecoder.telemetryDecoder.decode([PostAggregator].self, from: examplePostAggregatorExpression)
+
+        XCTAssertEqual(
+            decodedAggregators,
+            [
+                PostAggregator.arithmetic(.init(
+                    name: "part_percentage",
+                    function: .multiplication,
+                    fields: [
+                        .arithmetic(.init(
+                            name: "ratio",
+                            function: .division, fields: [
+                                .fieldAccess(.init(type: .fieldAccess, name: "part", fieldName: "part")),
+                                .fieldAccess(.init(type: .fieldAccess, name: "tot", fieldName: "tot"))
+                            ]
+                        )),
+                        PostAggregator.constant(.init(name: "const", value: 100))
+                    ]
+                ))
+            ]
+        )
+    }
+
+    func testPostAggregatorArithmeticDecoding() throws {
+        let examplePostAggregatorArithmetic = """
+        [{
+            "type"   : "arithmetic",
+            "name"   : "average",
+            "fn"     : "/",
+            "fields" : [
+                   { "type" : "fieldAccess", "name" : "tot", "fieldName" : "tot" },
+                   { "type" : "fieldAccess", "name" : "rows", "fieldName" : "rows" }
+                 ]
+          }]
+        """
+        .filter { !$0.isWhitespace }
+        .data(using: .utf8)!
+
+        let decodedAggregators = try JSONDecoder.telemetryDecoder.decode([PostAggregator].self, from: examplePostAggregatorArithmetic)
 
         XCTAssertEqual(decodedAggregators, [
             PostAggregator.arithmetic(.init(
@@ -123,7 +146,7 @@ final class PostAggregatorTests: XCTestCase {
             ))
         ])
     }
-    
+
     let exampleHyperUnique = """
     [{
         "type"   : "arithmetic",
@@ -137,7 +160,7 @@ final class PostAggregatorTests: XCTestCase {
     """
     .filter { !$0.isWhitespace }
     .data(using: .utf8)!
-    
+
     func testHyperUniqueDecoding() throws {
         let decodedAggregators = try JSONDecoder.telemetryDecoder.decode([PostAggregator].self, from: exampleHyperUnique)
 
