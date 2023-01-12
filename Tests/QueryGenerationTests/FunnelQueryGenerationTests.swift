1b0742b333351ed88bcd61956847351c32026319
Daniel Jilg <daniel@telemetrydeck.com>
2023-01-12 11:09:58 +0100

    Write first sketch of generated query

--- Tests/QueryGenerationTests/FunnelQueryGenerationTests.swift
+++ Tests/QueryGenerationTests/FunnelQueryGenerationTests.swift
@@ -2,8 +2,20 @@ import DataTransferObjects
 import XCTest
 
 final class FunnelQueryGenerationTests: XCTestCase {
-    
-    
+    let steps: [Filter] = [
+        .selector(.init(dimension: "type", value: "appLaunchedRegularly")),
+        .selector(.init(dimension: "type", value: "dataEntered")),
+        .selector(.init(dimension: "type", value: "paywallSeen")),
+        .selector(.init(dimension: "type", value: "conversion"))
+    ]
+
+    let stepNames: [String] = [
+        "Regular Launch",
+        "Data Entered",
+        "Paywall Presented",
+        "Conversion"
+    ]
+
     let tinyQuery = CustomQuery(
         queryType: .groupBy,
         dataSource: "telemetry-signals",
@@ -11,18 +23,20 @@ final class FunnelQueryGenerationTests: XCTestCase {
             .selector(.init(dimension: "appID", value: "79167A27-EBBF-4012-9974-160624E5D07B")),
             .selector(.init(dimension: "isTestMode", value: "false")),
             .or(.init(fields: [
-                .selector(.init(dimension: "type", value: "appLaunchedByNotification")),
-                .selector(.init(dimension: "type", value: "dataEntered"))
+                .selector(.init(dimension: "type", value: "appLaunchedRegularly")),
+                .selector(.init(dimension: "type", value: "dataEntered")),
+                .selector(.init(dimension: "type", value: "paywallSeen")),
+                .selector(.init(dimension: "type", value: "conversion"))
             ]))
         ])),
         granularity: .all,
         aggregations: [
             .filtered(.init(
-                filter: .selector(.init(dimension: "type", value: "appLaunchedByNotification")),
+                filter: .selector(.init(dimension: "type", value: "appLaunchedRegularly")),
                 aggregator: .thetaSketch(
                     .init(
                         type: AggregatorType.thetaSketch,
-                        name: "appLaunchedByNotification_count",
+                        name: "_funnel_step_0",
                         fieldName: "clientUser"
                     )
                 )
@@ -31,8 +45,28 @@ final class FunnelQueryGenerationTests: XCTestCase {
                 filter: .selector(.init(dimension: "type", value: "dataEntered")),
                 aggregator: .thetaSketch(
                     .init(
-                        type: .thetaSketch,
-                        name: "dataEntered_count",
+                        type: AggregatorType.thetaSketch,
+                        name: "_funnel_step_1",
+                        fieldName: "clientUser"
+                    )
+                )
+            )),
+            .filtered(.init(
+                filter: .selector(.init(dimension: "type", value: "paywallSeen")),
+                aggregator: .thetaSketch(
+                    .init(
+                        type: AggregatorType.thetaSketch,
+                        name: "_funnel_step_2",
+                        fieldName: "clientUser"
+                    )
+                )
+            )),
+            .filtered(.init(
+                filter: .selector(.init(dimension: "type", value: "conversion")),
+                aggregator: .thetaSketch(
+                    .init(
+                        type: AggregatorType.thetaSketch,
+                        name: "_funnel_step_3",
                         fieldName: "clientUser"
                     )
                 )
@@ -40,22 +74,77 @@ final class FunnelQueryGenerationTests: XCTestCase {
         ],
         postAggregations: [
             .thetaSketchEstimate(.init(
-                name: "app_launched_and_data_entered_count",
+                name: "0_Regular Launch",
                 field: .thetaSketchSetOp(.init(
                     func: .intersect,
                     fields: [
                         .fieldAccess(.init(
                             type: .fieldAccess,
-                            fieldName: "appLaunchedByNotification_count"
-                        )),
-                        .fieldAccess(.init(
-                            type: .fieldAccess,
-                            fieldName: "dataEntered_count"
+                            fieldName: "_funnel_step_0"
                         ))
                     ]
                 ))
-            )
-            )
+            )),
+            .thetaSketchEstimate(.init(
+                name: "1_Data Entered",
+                field: .thetaSketchSetOp(.init(
+                    func: .intersect,
+                    fields: [
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_0"
+                        )),
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_1"
+                        ))
+                    ]
+                ))
+            )),
+            .thetaSketchEstimate(.init(
+                name: "2_Paywall Presented",
+                field: .thetaSketchSetOp(.init(
+                    func: .intersect,
+                    fields: [
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_0"
+                        )),
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_1"
+                        )),
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_2"
+                        ))
+                    ]
+                ))
+            )),
+            .thetaSketchEstimate(.init(
+                name: "3_Conversion",
+                field: .thetaSketchSetOp(.init(
+                    func: .intersect,
+                    fields: [
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_0"
+                        )),
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_1"
+                        )),
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_2"
+                        )),
+                        .fieldAccess(.init(
+                            type: .fieldAccess,
+                            fieldName: "_funnel_step_3"
+                        ))
+                    ]
+                ))
+            ))
         ]
     )
 
@@ -66,9 +155,9 @@ final class FunnelQueryGenerationTests: XCTestCase {
             appID: "79167A27-EBBF-4012-9974-160624E5D07B",
             testMode: false
         )
-        
+
         XCTAssertEqual(tinyQuery, generatedTinyQuery)
-        
+
         XCTAssertEqual(String(data: try! JSONEncoder.telemetryEncoder.encode(tinyQuery), encoding: .utf8), String(data: try! JSONEncoder.telemetryEncoder.encode(generatedTinyQuery), encoding: .utf8))
     }
 }
