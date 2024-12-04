99decfbeda0b0bfe97d13ba7cd45db6c981e9050
Daniel Jilg <daniel@telemetrydeck.com>
2024-12-04 17:50:52 +0100

    Implement IdleConfigTests

--- Tests/SupervisorTests/IdleConfigTests.swift
+++ Tests/SupervisorTests/IdleConfigTests.swift
@@ -2,20 +2,25 @@
 import XCTest
 
 final class IdleConfigTests: XCTestCase {
-    func testDecodingDocsExample() throws {
-        XCTFail("not implemented")
+    let tdValueString = """
+    {
+        "enabled": true,
+        "inactiveAfterMillis": 10000
     }
+    """
+    .filter { !$0.isWhitespace }
+
+    let tdValue = IdleConfig(enabled: true, inactiveAfterMillis: 10000)
+
+    let testedType = IdleConfig.self
 
     func testDecodingTelemetryDeckExample() throws {
-        XCTFail("not implemented")
-    }
-
-    func testEncodingDocsExample() throws {
-        XCTFail("not implemented")
+        let decodedValue = try JSONDecoder.telemetryDecoder.decode(testedType, from: tdValueString.data(using: .utf8)!)
+        XCTAssertEqual(tdValue, decodedValue)
     }
 
     func testEncodingTelemetryDeckExample() throws {
-        XCTFail("not implemented")
+        let encodedValue = try JSONEncoder.telemetryEncoder.encode(tdValue)
+        XCTAssertEqual(tdValueString, String(data: encodedValue, encoding: .utf8)!)
     }
-
 }
