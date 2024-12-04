41b1854db7c9af7aeb05f2cc7c203b7f81b4c708
Daniel Jilg <daniel@telemetrydeck.com>
2024-12-04 17:43:46 +0100

    Implement InputFormatTests

--- Tests/SupervisorTests/InputFormatTests.swift
+++ Tests/SupervisorTests/InputFormatTests.swift
@@ -2,20 +2,44 @@
 import XCTest
 
 final class InputFormatTests: XCTestCase {
-    func testDecodingDocsExample() throws {
-        XCTFail("not implemented")
+    let docsValueString = """
+    {
+        "type": "json"
     }
+    """
+    .filter { !$0.isWhitespace }
 
-    func testDecodingTelemetryDeckExample() throws {
-        XCTFail("not implemented")
+    let docsValue = InputFormat(type: .json, keepNullColumns: nil)
+
+    let tdValueString = """
+    {
+        "keepNullColumns": false,
+        "type": "json"
+    }
+    """
+    .filter { !$0.isWhitespace }
+
+    let tdValue = InputFormat(type: .json, keepNullColumns: false)
+
+    let testedType = InputFormat.self
+
+    func testDecodingDocsExample() throws {
+        let decodedValue = try JSONDecoder.telemetryDecoder.decode(testedType, from: docsValueString.data(using: .utf8)!)
+        XCTAssertEqual(docsValue, decodedValue)
     }
 
     func testEncodingDocsExample() throws {
-        XCTFail("not implemented")
+        let encodedValue = try JSONEncoder.telemetryEncoder.encode(docsValue)
+        XCTAssertEqual(docsValueString, String(data: encodedValue, encoding: .utf8)!)
+    }
+
+    func testDecodingTelemetryDeckExample() throws {
+        let decodedValue = try JSONDecoder.telemetryDecoder.decode(testedType, from: tdValueString.data(using: .utf8)!)
+        XCTAssertEqual(tdValue, decodedValue)
     }
 
     func testEncodingTelemetryDeckExample() throws {
-        XCTFail("not implemented")
+        let encodedValue = try JSONEncoder.telemetryEncoder.encode(tdValue)
+        XCTAssertEqual(tdValueString, String(data: encodedValue, encoding: .utf8)!)
     }
 }
-
