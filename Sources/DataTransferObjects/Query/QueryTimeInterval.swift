8949edd1d93e75f5ce7dbf5bd5b7f23758d288fe
Daniel Jilg <daniel@telemetrydeck.com>
2025-04-25 15:25:49 +0200

    implement methods of generating new time intervals from old timeintervals minus time segments

--- Sources/DataTransferObjects/Query/QueryTimeInterval.swift
+++ Sources/DataTransferObjects/Query/QueryTimeInterval.swift
@@ -6,13 +6,79 @@ public struct QueryTimeIntervalsContainer: Codable, Hashable, Equatable {
         case intervals
     }
 
+    public init(type: QueryTimeIntervalsContainer.ContainerType, intervals: [QueryTimeInterval]) {
+        self.type = type
+        self.intervals = intervals
+    }
+
+    public init(type: QueryTimeIntervalsContainer.ContainerType, timeSegments: [TimeSegment]) throws {
+        self.type = type
+
+        // Sort and deduplicate time segments
+        let timeSegmentsNormalized = Set(timeSegments).sorted { $0.beginningDate < $1.beginningDate }
+
+        if timeSegmentsNormalized.isEmpty {
+            self.intervals = []
+            return
+        }
+
+        var component: Calendar.Component
+        switch timeSegmentsNormalized.first!.duration {
+        case .hour:
+            component = .hour
+        case .day:
+            component = .day
+        case .month:
+            component = .month
+        case .year:
+            component = .year
+        default:
+            throw CustomQuery.QueryGenerationError.compilationStatusError
+        }
+
+        var calendar = Calendar(identifier: .iso8601)
+        calendar.timeZone = TimeZone(identifier: "UTC")!
+
+        var intervals = [QueryTimeInterval]()
+        var currentInterval = QueryTimeInterval(beginningDate: timeSegmentsNormalized.first!.beginningDate, endDate: timeSegmentsNormalized.first!.beginningDate)
+
+        for segment in timeSegmentsNormalized {
+            // If the segment is not continouus, create a new interval
+            if try segment.beginningDate > calendar.add(component, to: currentInterval.endDate) {
+                currentInterval.endDate = try calendar.add(component, to: currentInterval.endDate)
+                intervals.append(currentInterval)
+                currentInterval = QueryTimeInterval(beginningDate: segment.beginningDate, endDate: segment.beginningDate)
+            }
+
+            // Extend the current interval
+            if segment.beginningDate > currentInterval.endDate {
+                currentInterval.endDate = segment.beginningDate
+            }
+        }
+        currentInterval.endDate = try calendar.add(component, to: currentInterval.endDate)
+        intervals.append(currentInterval)
+
+        self.intervals = intervals
+    }
+
     public let type: ContainerType
     public let intervals: [QueryTimeInterval]
+
+    public func timeSegments(with granularity: QueryGranularity) throws -> [TimeSegment] {
+        var segments = Set<TimeSegment>()
+
+        for interval in intervals {
+            let intervalSegments = try interval.timeSegments(with: granularity)
+            segments.formUnion(intervalSegments)
+        }
+
+        return segments.sorted { $0.beginningDate < $1.beginningDate }
+    }
 }
 
 public struct QueryTimeInterval: Codable, Hashable, Equatable, Comparable {
-    public let beginningDate: Date
-    public let endDate: Date
+    public var beginningDate: Date
+    public var endDate: Date
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
@@ -109,7 +175,7 @@ public struct QueryTimeInterval: Codable, Hashable, Equatable, Comparable {
         }
 
         while currentDate < endDate {
-            let nextDate = calendar.date(byAdding: component, value: 1, to: currentDate)!
+            let nextDate = try calendar.add(component, to: currentDate)
             let segment = TimeSegment(beginningDate: currentDate, duration: granularity)
             segments.append(segment)
             currentDate = nextDate
@@ -127,4 +193,13 @@ public struct TimeSegment: Codable, Hashable, Equatable {
 
     public let beginningDate: Date
     public let duration: QueryGranularity
+}
+
+extension Calendar {
+    func add(_ component: Calendar.Component, value: Int = 1, to date: Date) throws -> Date {
+        guard let result = self.date(byAdding: component, value: value, to: date) else {
+            throw CustomQuery.QueryGenerationError.compilationStatusError
+        }
+        return result
+    }
 }
--- Tests/QueryTests/QueryTimeIntervalTests.swift
+++ Tests/QueryTests/QueryTimeIntervalTests.swift
@@ -38,4 +38,60 @@ final class QueryTimeIntervalTests: XCTestCase {
             ]
         )
     }
+
+    func testQueryTimeIntervalsContainerTimeSegments() throws {
+        let timeIntervalContainer = QueryTimeIntervalsContainer(type: .intervals, intervals: [
+            QueryTimeInterval(
+                beginningDate: Date(iso8601String: "2022-07-28T17:21:00.000Z")!,
+                endDate: Date(iso8601String: "2022-08-01T11:30:00.000Z")!
+            ),
+            QueryTimeInterval(
+                beginningDate: Date(iso8601String: "2022-07-29T17:21:00.000Z")!,
+                endDate: Date(iso8601String: "2022-08-03T11:30:00.000Z")!
+            ),
+        ])
+
+        let granularity = QueryGranularity.day
+        let segments = try timeIntervalContainer.timeSegments(with: granularity)
+        XCTAssertEqual(
+            segments,
+            [
+                .init(beginningDate: .init(iso8601String: "2022-07-28T00:00:00.000Z")!, duration: .day),
+                .init(beginningDate: .init(iso8601String: "2022-07-29T00:00:00.000Z")!, duration: .day),
+                .init(beginningDate: .init(iso8601String: "2022-07-30T00:00:00.000Z")!, duration: .day),
+                .init(beginningDate: .init(iso8601String: "2022-07-31T00:00:00.000Z")!, duration: .day),
+                .init(beginningDate: .init(iso8601String: "2022-08-01T00:00:00.000Z")!, duration: .day),
+                .init(beginningDate: .init(iso8601String: "2022-08-02T00:00:00.000Z")!, duration: .day),
+                .init(beginningDate: .init(iso8601String: "2022-08-03T00:00:00.000Z")!, duration: .day),
+            ]
+        )
+    }
+
+    func testQueryTimeIntervalsContainerInitWithTimeSegments() throws {
+        let timeIntervalContainer = try QueryTimeIntervalsContainer(type: .intervals, timeSegments: [
+            .init(beginningDate: .init(iso8601String: "2022-07-28T00:00:00.000Z")!, duration: .day),
+            .init(beginningDate: .init(iso8601String: "2022-07-29T00:00:00.000Z")!, duration: .day),
+            // missing a day on purpose
+            .init(beginningDate: .init(iso8601String: "2022-07-31T00:00:00.000Z")!, duration: .day),
+            .init(beginningDate: .init(iso8601String: "2022-08-01T00:00:00.000Z")!, duration: .day),
+            .init(beginningDate: .init(iso8601String: "2022-08-02T00:00:00.000Z")!, duration: .day),
+            .init(beginningDate: .init(iso8601String: "2022-08-03T00:00:00.000Z")!, duration: .day),
+        ])
+
+        XCTAssertEqual(
+            timeIntervalContainer.intervals,
+            [
+                // Intervals' beginnings are inclusive, and the endings are exclusive
+                .init(
+                    beginningDate: .init(iso8601String: "2022-07-28T00:00:00.000Z")!,
+                    endDate: .init(iso8601String: "2022-07-30T00:00:00.000Z")!
+                ),
+
+                .init(
+                    beginningDate: .init(iso8601String: "2022-07-31T00:00:00.000Z")!,
+                    endDate: .init(iso8601String: "2022-08-04T00:00:00.000Z")!
+                ),
+            ]
+        )
+    }
 }
